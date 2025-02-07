8bff78b9a4b7b2efbabcdd7635e44337a1c1df18
VHemanth45 <you@example.comvhemantnaik45@gmail.com>
2025-02-07 22:21:48 +0530

    Created a directory App and created a databaseschema

---
+++ App/databasedef.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    status = db.Column(db.String(20), nullable=False, default='PENDING')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'status': self.status,
+            'createdAt': self.created_at.isoformat() + 'Z'
+        }
